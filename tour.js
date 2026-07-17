60f051504a9997a8c83ae13ec1da2458f2b0fe4e
woz3323 <woz3323@gmail.com>
2026-07-16 22:40:56 -0500

    Tour: change final CTA to 'I'm ready to help'

--- tour.js
+++ tour.js
@@ -90,7 +90,7 @@
       title: '📬 Contact',
       body: 'Have questions or want to get more involved? Reach out and we are here to help.',
       link: 'contact-us.html',
-      nextLabel: 'Thanks for looking around!'
+      nextLabel: "I'm ready to help →"
     }
   ];
 
